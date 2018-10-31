4dd72c95104f43215a60ec5f334aac03bece0353
West Kraemer <kraemerwest@gmail.com>
2018-10-31 11:52:49 -0400

    remove extensions from root class and add configure location services function to viewDidLoad. Need to still update info p list file to get user permission.

--- mapsProject/MapVC.swift
+++ mapsProject/MapVC.swift
@@ -14,17 +14,35 @@ class MapVC: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    var locationManager = CLLocationManager()
+    let authorizationStatus = CLLocationManager.authorizationStatus()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        mapView.delegate = self as! MKMapViewDelegate
+        mapView.delegate = self
+        locationManager.delegate = self
+        configureLocationServices()
     }
 
     @IBAction func centerMapBtnWasPressed(_ sender: Any) {
     }
     
-//    extension MapVC: MKMapViewDelegate {
-//
-//    }
-    
+
+
 }
 
+
+    extension MapVC: MKMapViewDelegate {
+
+    }
+
+
+extension MapVC: CLLocationManagerDelegate {
+    func configureLocationServices() {
+        if authorizationStatus == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        } else {
+            return
+        }
+    }
+}
